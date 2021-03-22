fbf9d7ec2cab24cffe21272609db14d51b5a7b23
iamjunsulee <mellow0924@gmail.com>
2021-03-22 20:29:23 +0900

    초기 데이터 sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '자바 ORM 표준 JPA 프로그래밍', 43000, 10);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '달러구트 꿈 백화점', 14000, 15);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '비극의 탄생', 19000, 4);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '질서 너머', 17000, 0);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '2030 축의 전환', 15000, 3);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '흔한 남매7', 10000, 1);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '주택과 세금', 26000, 2);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '나의 첫 투자 수업1', 18000, 15);
+insert into item(item_id, name, price, stock_quantity) values (item_seq.nextval, '나의 첫 투자 수업2', 18000, 10);
