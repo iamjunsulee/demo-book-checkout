f9ac3ae10d3f395fb967722274a56c495956f639
iamjunsulee <mellow0924@gmail.com>
2021-03-29 23:39:16 +0900

    OrderItem 생성 메서드 추가

--- src/main/java/me/junsu/book/domain/OrderItem.java
+++ src/main/java/me/junsu/book/domain/OrderItem.java
@@ -3,6 +3,7 @@ package me.junsu.book.domain;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.aspectj.weaver.ast.Or;
 
 import javax.persistence.*;
 
@@ -25,4 +26,13 @@ public class OrderItem {
 
     private int orderQuantity;  //주문수량
     private int orderPrice;     //주문가격
+
+    /* 생성메서드 */
+    public static OrderItem createOrderItem(Item item, int count, int price) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setOrderPrice(price);
+        orderItem.setItem(item);
+        orderItem.setOrderQuantity(count);
+        return orderItem;
+    }
 }
